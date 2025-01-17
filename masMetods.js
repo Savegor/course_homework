9ab7a5c5b2e2d54f53441abcabdb9dc6593def3c
Egor <egorsavvinovv2003@yandex.ru>
2025-01-17 18:24:50 +1000

    add new metods

--- masMetods.js
+++ masMetods.js
@@ -10,10 +10,45 @@ Array.prototype.mySlice = function(start, end=null){
     return newMas
 }
 
-Array.prototype.myIndexOf = function(arr, item, from){
-
+Array.prototype.myIndexOf = function(searchElement , fromIndex=0){
+    for (let i=fromIndex; i < this.length; i++) {
+        if (this[i] === searchElement) return i
+    }
+    return -1
 }
 
-Array.prototype.myIncludes = function(arr, item, from){
-
+Array.prototype.myIncludes = function(searchElement , fromIndex=0){
+    for (let i=fromIndex; i < this.length; i++) {
+        if (this[i] === searchElement) return true
+    }
+    return false
 }
+
+Array.prototype.mySplice = function(start, deleteCount=0, ...elements){
+    let newMas = []
+    if(start < 0 && (start *-1) <= this.length) start = this.length - ((start *-1))
+    else if ((start *-1) > this.length) start = 0
+    for (let i=0; i < start; i++) newMas.push(this[i])
+    newMas.push(...elements)
+    for (let i=start; i < this.length; i++){
+        if (deleteCount === 0){
+            newMas.push(this[i])
+        }
+        else deleteCount--
+    }
+    return newMas
+}
+
+Array.prototype.myConcat = function(...arg){
+    let newMas = this
+    arg.forEach((e, i) => {
+        if (typeof e === "object"){
+            e.forEach((elem) => {newMas.push(elem)})
+        }
+        else newMas.push(e)
+    })
+    return newMas
+}
+let mas = [1,2,3,4]
+// console.log("JS ", mas.concat([2,5], 6, [43, [1]]))
+console.log("MY :", mas.myConcat([2,5], 6, [43, [1]]))
